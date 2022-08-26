298adc621f9a65606dce36fa66ed4fc6b90592f2
Dion Dokter <diondokter@gmail.com>
2022-08-26 17:15:40 +0200

    Tcp testing

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@
 use defmt::unwrap;
 use defmt_rtt as _;
 use embassy_nrf::interrupt::{self, InterruptExt, Priority};
-use futures::StreamExt;
+use nrf_modem::lte_link::LteLink;
+use nrf_modem::no_std_net::SocketAddr;
 use nrf_modem::{
-    gnss::{GnssConfig, NmeaMask},
     ConnectionPreference, SystemMode,
 };
 use panic_probe as _;
 
 extern crate tinyrlibc;
+
+defmt::timestamp!("{=u64:us}", {
+    embassy::time::Instant::now().as_micros()
+});
 
 #[embassy::main]
 async fn main(_spawner: embassy::executor::Spawner, _p: embassy_nrf::Peripherals) {
@@ -43,54 +47,64 @@ async fn main(_spawner: embassy::executor::Spawner, _p: embassy_nrf::Peripherals
     });
     ipc.enable();
 
+    let regulators: embassy_nrf::pac::REGULATORS = unsafe { core::mem::transmute(()) };
+    regulators.dcdcen.modify(|_, w| w.dcdcen().enabled());
+
     run().await;
 
     exit();
 }
 
 async fn run() {
-    // let mut cp = unwrap!(cortex_m::Peripherals::take());
-    // let _dp = unwrap!(nrf9160_hal::pac::Peripherals::take());
-
     defmt::println!("Initializing modem");
     unwrap!(
         nrf_modem::init(SystemMode {
             lte_support: true,
-            nbiot_support: true,
+            nbiot_support: false,
             gnss_support: true,
-            preference: ConnectionPreference::NetworkPreferenceWithLteFallback,
+            preference: ConnectionPreference::Lte,
         })
         .await
     );
 
-    // nrf_modem::at::send_at("AT+CEREG=?").await.unwrap();
-    nrf_modem::at::send_at("AT+CEREG=1").await.unwrap();
-    loop {
-        let notif = nrf_modem::at_notifications::wait_for_at_notification::<256>().await;
-        defmt::println!("{}", notif.as_str());
-    }
+    // nrf_modem::configure_gnss_on_pca10090ns().await.unwrap();
 
-    unwrap!(nrf_modem::configure_gnss_on_pca10090ns().await);
-    defmt::println!("Initializing gps");
-    let mut gnss = nrf_modem::gnss::Gnss::new().await.unwrap();
-    defmt::println!("Starting single fix");
-    let mut iter = gnss
-        .start_continuous_fix(GnssConfig {
-            fix_retry: 600,
-            nmea_mask: NmeaMask {
-                gga: false,
-                gll: false,
-                gsa: false,
-                gsv: false,
-                rmc: false,
-            },
-            ..Default::default()
-        })
+    // defmt::println!("Initializing gps");
+    // let mut gnss = nrf_modem::gnss::Gnss::new().await.unwrap();
+    // defmt::println!("Starting single fix");
+    // let mut iter = gnss
+    //     .start_continuous_fix(GnssConfig {
+    //         fix_retry: 600,
+    //         nmea_mask: NmeaMask {
+    //             gga: false,
+    //             gll: false,
+    //             gsa: false,
+    //             gsv: false,
+    //             rmc: false,
+    //         },
+    //         ..Default::default()
+    //     })
+    //     .unwrap();
+
+    // while let Some(x) = iter.next().await {
+    //     defmt::println!("{:?}", defmt::Debug2Format(&x));
+    // }
+
+    let link = LteLink::new().await.unwrap();
+    link.wait_for_link().await.unwrap();
+    let google_ip = nrf_modem::dns::get_host_by_name("google.com").unwrap();
+    defmt::println!("Google ip: {:?}", defmt::Debug2Format(&google_ip));
+    let stream = nrf_modem::tcp_stream::TcpStream::connect(SocketAddr::from((google_ip, 80)))
+        .await
         .unwrap();
 
-    while let Some(x) = iter.next().await {
-        defmt::println!("{:?}", defmt::Debug2Format(&x));
-    }
+    stream.send("GET / HTTP/1.0\nHost: google.com\r\n\r\n".as_bytes()).await.unwrap();
+    let mut buffer = [0; 1024];
+    let used = stream.receive(&mut buffer).await.unwrap();
+
+    defmt::println!("Google page: {}", core::str::from_utf8(used).unwrap());
+
+    exit();
 }
 
 // same panicking *behavior* as `panic-probe` but doesn't print a panic message
